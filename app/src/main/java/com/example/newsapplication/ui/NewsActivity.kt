23d426ea66805d1d7bdddd11583f60cad78a037b
vikaslohar21 <vikaslohar2122@gmail.com>
2024-07-16 14:38:21 +0530

    Fragments and Final Output Pending!!

--- app/src/main/java/com/example/newsapplication/ui/NewsActivity.kt
+++ app/src/main/java/com/example/newsapplication/ui/NewsActivity.kt
@@ -2,11 +2,31 @@ package com.example.newsapplication.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.newsapplication.R
+import com.example.newsapplication.databinding.ActivityNewsBinding
+import com.example.newsapplication.db.ArticleDatabase
+import com.example.newsapplication.models.Article
+import com.example.newsapplication.repository.NewsRepository
 
 class NewsActivity : AppCompatActivity() {
+
+    lateinit var newsViewModel: NewsViewModel
+    lateinit var binding: ActivityNewsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(application, newsRepository)
+        newsViewModel =
+            ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
